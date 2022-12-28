912a0971dcc753e35c0fa4d4cc10a387a89bb86f
zkatrn <kathrynrandrade@gmail.com>
2022-12-28 15:06:45 -0800

    1 file modified

--- src/pagination/index.tsx
+++ src/pagination/index.tsx
@@ -10,13 +10,30 @@ const Pagination = ({
   const [pageList, setPageList] = useState<number[]>([]);
 
   useEffect(() => {
+    /**
+     * This `start` and `end` will handle pages in the
+     * middle of the array, if `currentPage` is
+     * greater than 5 and less than totalPages - 4
+     */
     let start = currentPage - 1;
     let end = currentPage + 1;
 
     if (currentPage < 5) {
+      /**
+       * If currentPage is at the beginning of the page list,
+       * we want to show (1), 2, 3, 4, 5 so we set the start
+       * and end of the array as 2 and 5
+       */
       start = 2;
       end = 5;
     } else if (currentPage > totalPages - 4) {
+      /**
+       * Similarly with the start of the array, we want
+       * to handle the end of the page list,
+       * we want to show 16, 17, 18, 19, (20), so we
+       * set the start to 16 in this case and
+       * end with  19 (totalPages - 4 = 16, totalPages - 1 = 19)
+       */
       start = totalPages - 4;
       end = totalPages - 1;
     }
@@ -24,11 +41,7 @@ const Pagination = ({
     const _pageList = [];
 
     for (let i = start; i <= end; i++) {
-      if (i !== 1 && i !== totalPages) {
-        if (i > 0 && i < totalPages + 1) {
-          _pageList.push(i);
-        }
-      }
+      _pageList.push(i);
     }
 
     setPageList(_pageList);
